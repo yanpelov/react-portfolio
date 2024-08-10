063f82f3886d2b5d1be687c618d573ccafa86ed5
yanpelov <11660378+yanpelov@users.noreply.github.com>
2024-08-10 11:43:36 +0300

    added onLoad to wait for the map to load before identifing center for player sprite

--- app/constants.tsx
+++ app/constants.tsx
@@ -10,42 +10,46 @@ export type TextWithPhoto = {
 
 export const content: Record<string, TextWithPhoto> = {
     intro: {
-        text: `Welcome, fearless recruiter! You’ve stepped into CodeLand, where every line of code tells a story and every bug is a dragon to be slayed. Ready to embark on an epic quest to uncover the legendary developer’s skills and achievements? Let the adventure begin!`,
-        photoUrl: '' // Replace with actual path
+        text: `Welcome, fearless stranger! You’ve stepped into CodeLand, where every line of code tells a story and every bug is a dragon to be slayed. Ready to embark on an epic quest to uncover the legendary developer’s skills and achievements? Let the adventure begin!`,
+        photoUrl: ''
     },
     saucelabs: {
         text: `Behold the legendary Sauce Labs cabinet! It's not just any sauce — it’s been tested on humans (and their taste buds). Which one will you choose? (My favorite onboarding question — it’s saucy, I know!) <span style="color:green">Rest assured, all sauces have been thoroughly tested.</span>`,
-        photoUrl: '/sl_logo.png' // Replace with actual path
+        photoUrl: '/sl_logo.png'
     },
     audiocodes: {
         text: `Welcome to the AudioCodes realm! Here, you’ll find the signal wave of the sound of... what’s this? A sin of sinosatis? Sounds like a plot twist in a sci-fi novel! Or maybe just a really complicated sound wave.`,
-        photoUrl: '/ac_logo.png' // Optional, can be empty
+        photoUrl: '/ac_logo.png'
     },
     beeriprint: {
         text: `Ever wondered how many mailboxes a Be'eri Printers can print? Let’s just say it’s a mailbox bonanza. Beeri Printers: where mailboxes are printed in bulk, just like your favorite beer is brewed!`,
-        photoUrl: '/dfus_logo.png' // Replace with actual path
+        photoUrl: '/dfus_logo.png'
     },
 
     uni: {
         text: `I graduated law. I came, I saw, I left for CS `,
-        photoUrl: '/heb_logo.png' // Replace with actual path
+        photoUrl: '/heb_logo.png'
     },
 
     infinity: {
         text: `Ever wondered how many mailboxes a Be'eri Printers can print? Let’s just say it’s a mailbox bonanza. Beeri Printers: where mailboxes are printed in bulk, just like your favorite beer is brewed!`,
-        photoUrl: '/infi_logo.png' // Replace with actual path
+        photoUrl: '/infi_logo.png'
     },
     leetcode: {
         text: `LeetCode is like a rollercoaster for your brain — thrilling, challenging, and occasionally mind-bending. Your mind might go kaboom, just like the confetti in the photo frame. Buckle up and enjoy the ride!`,
-        photoUrl: '/lcpxl.png' // Optional, can be empty
+        photoUrl: '/leetcode.png'
     },
     cv: {
         text: `Congratulations on making it this far! You’re about to be redirected — it’s like the suspenseful countdown before the big reveal. 3... 2... 1... Prepare for the grand finale!`,
-        photoUrl: '' // Optional, can be empty
+        photoUrl: ''
     },
     contact: {
         text: `Well done! You remembered to turn off the lights before leaving. Now, if you want to keep in touch, here are my contacts. No need for a flashlight — just reach out!`,
-        photoUrl: '' // Replace with actual path
+        photoUrl: ''
+    },
+    archive: {
+        text: `Well done! You remembered to turn off the lights before leaving. Now, if you want to keep in touch, here are my contacts. No need for a flashlight — just reach out!`,
+        photoUrl: 'archive.svg'
     }
 };
 
--- app/kaboom/KaboomComponent.tsx
+++ app/kaboom/KaboomComponent.tsx
@@ -9,9 +9,9 @@ const KaboomComponent: React.FC = () => {
     const canvasRef = useRef<HTMLCanvasElement | null>(null);
     const [isVisible, setIsVisible] = useState(false);
     const [dialogueText, setDialogueText] = useState("");
-    const [dialogueImg, setDialogueImg] = useState("")
+    const [dialogueImg, setDialogueImg] = useState("");
     const [dialogueCallback, setDialogueCallback] = useState<(() => void) | null>(null);
-    const router = useRouter()
+    const router = useRouter();
 
     // Define type for valid keys
     type ContentKeys = keyof typeof content;
@@ -21,69 +21,56 @@ const KaboomComponent: React.FC = () => {
         return key in content;
     };
 
-    // Function to get text and photo URL based on key
-    const getContent = (key: string): TextWithPhoto => {
-        if (isValidKey(key)) {
-            return content[key];
-        } else {
-            return {}; // Default to an empty object when key is invalid
-        }
-    };
+    const getContent = (key: string): TextWithPhoto => content[key] || {};
 
     const closeDialogue = () => {
         setIsVisible(false);
         setDialogueText("");
-        setDialogueImg("")
+        setDialogueImg("");
         dialogueCallback?.();
     };
 
     const displayDialogue = (text: string, cb: (() => void) | null) => {
-
         let i = 0;
-        let showed = ""
+        let showed = "";
         const typeChars = () => {
             if (i < text.length) {
                 showed += text[i++];
                 setDialogueText(showed);
-                setTimeout(typeChars, 10)
+                setTimeout(typeChars, 10);
             }
-        }
+        };
 
-        typeChars()
+        typeChars();
         setIsVisible(true);
-
         setDialogueCallback(() => cb);
-
-    }
+    };
 
     const propAction = (player: GameObj<SpriteComp | PosComp | ScaleComp | AreaComp | BodyComp | AnchorComp | { speed: number; direction: string; isInDialogue: boolean; }>, propName: string) => {
         player.onCollide(propName, () => {
-
             if (!player.isInDialogue) {
                 player.isInDialogue = true;
                 if (propName === "cv") {
                     setTimeout(() => {
-                        router.push('/cv')
-                    }, 5000)
+                        router.push('/cv');
+                    }, 5000);
                 }
 
                 const { text = '', photoUrl = '' }: TextWithPhoto = content[propName] || {};
-                setDialogueImg(photoUrl)
+                setDialogueImg(photoUrl);
                 displayDialogue(text, () => {
-
                     player.isInDialogue = false;
                 });
             }
         });
-    }
-
+    };
 
     useEffect(() => {
         if (canvasRef.current) {
             const k: KaboomCtx = kaboom({
                 touchToMouse: true,
                 canvas: canvasRef.current,
-                background: [0, 0, 0]
+                background: [0, 0, 0],
             });
 
             k.loadFont("monogram", "/monogram.ttf");  // Load monogram font
@@ -95,14 +82,10 @@ const KaboomComponent: React.FC = () => {
                     "idle-up": 1014,
                     "idle-left": 1053,
                     "idle-right": 975,
-
                     "walk-down": { from: 936, to: 939, loop: true, speed: 8 },
                     "walk-up": { from: 1014, to: 1017, loop: true, speed: 8 },
                     "walk-left": { from: 1053, to: 1056, loop: true, speed: 8 },
                     "walk-right": { from: 975, to: 978, loop: true, speed: 8 },
-
-
-
                 },
             });
             k.loadSprite('map', "../map.png");
@@ -113,163 +96,150 @@ const KaboomComponent: React.FC = () => {
                     const layers = mapData.layers;
 
                     const map = k.add([k.sprite("map"), k.pos(0), k.scale(scaleFactor)]);
+                    k.onLoad(() => {
+                        // Calculate the center of the map
+                        const mapCenterX = map.width * scaleFactor / 2;
+                        const mapCenterY = map.height * scaleFactor / 2;
 
-                    // Calculate the center of the map
-                    const mapCenterX = map.width * scaleFactor / 2;
-                    const mapCenterY = map.height * scaleFactor / 2;
+                        const player = k.add([
+                            k.sprite("spritesheet", { anim: "idle-down" }),
+                            k.area({
+                                shape: new k.Rect(k.vec2(0, 3), 10, 10),
+                            }),
+                            k.body(),
+                            k.anchor("center"),
+                            k.pos(mapCenterX, mapCenterY),  // Position player at the map's center
+                            k.scale(scaleFactor),
+                            {
+                                speed: playerSpeed,
+                                direction: "walk-down",
+                                isInDialogue: false,
+                            },
+                            "player",
+                        ]);
 
-                    const player = k.add([
-                        k.sprite("spritesheet", { anim: "idle-down" }),
-                        k.area({
-                            shape: new k.Rect(k.vec2(0, 3), 10, 10),
-                        }),
-                        k.body(),
-                        k.anchor("center"),
-                        k.pos(mapCenterX, mapCenterY),  // Position player at the map's center
-                        k.scale(scaleFactor),
-                        {
-                            speed: playerSpeed,
-                            direction: "walk-down",
-                            isInDialogue: false,
-                        },
-                        "player",
-                    ]);
+                        layers.forEach((layer: { name: string; objects: any[]; }) => {
+                            if (layer.name === "walls") {
+                                layer.objects.forEach(boundary => {
+                                    map.add([
+                                        k.area({
+                                            shape: new k.Rect(k.vec2(0), boundary.width, boundary.height),
+                                        }),
+                                        k.body({ isStatic: true }),
+                                        k.pos(boundary.x, boundary.y),
+                                        boundary.name,
+                                    ]);
+                                });
+                            }
 
-                    layers.forEach((layer: { name: string; objects: any[]; }) => {
-                        if (layer.name === "walls") {
-                            layer.objects.forEach(boundary => {
-                                map.add([
-                                    k.area({
-                                        shape: new k.Rect(k.vec2(0), boundary.width, boundary.height),
-                                    }),
-                                    k.body({ isStatic: true }),
-                                    k.pos(boundary.x, boundary.y),
-                                    boundary.name,
-                                ]);
-                            });
-                        }
+                            if (layer.name === "props") {
+                                layer.objects.forEach(prop => {
+                                    map.add([
+                                        k.area({
+                                            shape: new k.Rect(k.vec2(0), prop.width, prop.height),
+                                        }),
+                                        k.body({ isStatic: true }),
+                                        k.pos(prop.x, prop.y),
+                                        prop.name,
+                                    ]);
+                                });
+                            }
 
-                        if (layer.name === "props") {
-                            layer.objects.forEach(prop => {
-                                map.add([
-                                    k.area({
-                                        shape: new k.Rect(k.vec2(0), prop.width, prop.height),
-                                    }),
-                                    k.body({ isStatic: true }),
-                                    k.pos(prop.x, prop.y),
-                                    prop.name,
-                                ]);
-                            });
-                        }
+                            if (layer.name === "props") {
+                                layer.objects.forEach(prop => {
+                                    propAction(player, prop.name);
+                                });
+                            }
+                        });
 
-                        if (layer.name === "props") {
-                            layer.objects.forEach(prop => {
+                        k.onUpdate(() => {
+                            k.camPos(player.pos.x, player.pos.y + 100);
+                        });
 
+                        k.onKeyRelease(() => {
+                            switch (player.direction) {
+                                case "walk-down":
+                                    player.play("idle-down");
+                                    break;
+                                case "walk-up":
+                                    player.play("idle-up");
+                                    break;
+                                case "walk-left":
+                                    player.play("idle-left");
+                                    break;
+                                case "walk-right":
+                                    player.play("idle-right");
+                                    break;
+                            }
+                        });
 
-                                propAction(player, prop.name)
+                        k.onKeyDown((key) => {
+                            if (player.isInDialogue) return;
+                            switch (key) {
+                                case "left":
+                                    player.move(-player.speed, 0);
+                                    player.direction = "walk-left";
+                                    break;
+                                case "right":
+                                    player.move(player.speed, 0);
+                                    player.direction = "walk-right";
+                                    break;
+                                case "up":
+                                    player.move(0, -player.speed);
+                                    player.direction = "walk-up";
+                                    break;
+                                case "down":
+                                    player.move(0, player.speed);
+                                    player.direction = "walk-down";
+                                    break;
+                            }
 
-                            });
-                        }
-                    });
+                            if (player.curAnim() !== player.direction) {
+                                player.play(player.direction);
+                            }
+                        });
 
-                    k.onUpdate(() => {
-                        k.camPos(player.pos.x, player.pos.y + 100);
-                    });
+                        k.onMouseDown((mouseBtn) => {
+                            if (mouseBtn !== "left" || player.isInDialogue) return;
 
-                    k.onKeyRelease(() => {
-                        switch (player.direction) {
-                            case "walk-down":
-                                player.play("idle-down");
-                                break;
-                            case "walk-up":
-                                player.play("idle-up");
-                                break;
-                            case "walk-left":
-                                player.play("idle-left");
-                                break;
-                            case "walk-right":
-                                player.play("idle-right");
-                                break;
-                        }
+                            const worldMousePos = k.toWorld(k.mousePos());
+                            const diff = worldMousePos.sub(player.pos);
+
+                            if (Math.abs(diff.x) > Math.abs(diff.y)) {
+                                player.direction = diff.x > 0 ? "walk-right" : "walk-left";
+                            } else {
+                                player.direction = diff.y > 0 ? "walk-down" : "walk-up";
+                            }
+
+                            if (player.curAnim() !== player.direction) {
+                                player.play(player.direction);
+                            }
+                            player.moveTo(worldMousePos, player.speed);
+                        });
+
+                        k.onMouseRelease(() => {
+                            switch (player.direction) {
+                                case "walk-down":
+                                    player.play("idle-down");
+                                    break;
+                                case "walk-up":
+                                    player.play("idle-up");
+                                    break;
+                                case "walk-left":
+                                    player.play("idle-left");
+                                    break;
+                                case "walk-right":
+                                    player.play("idle-right");
+                                    break;
+                            }
+                        });
+
+                        const { text = '', photoUrl = '' }: TextWithPhoto = content.intro || {};
+
+                        displayDialogue(text, () => {
+                            player.isInDialogue = false;
+                        });
                     })
-
-                    k.onKeyDown((key) => {
-                        if (player.isInDialogue) return;
-                        switch (key) {
-                            case "left":
-
-                                player.move(-player.speed, 0);
-                                player.direction = "walk-left";
-
-                                break;
-                            case "right":
-                                player.move(player.speed, 0);
-                                player.direction = "walk-right";
-
-                                break;
-                            case "up":
-                                player.move(0, -player.speed);
-                                player.direction = "walk-up";
-
-                                break;
-                            case "down":
-                                player.move(0, player.speed);
-                                player.direction = "walk-down";
-
-                                break;
-                        }
-
-                        if (player.curAnim() !== player.direction) {
-                            player.play(player.direction);
-                        }
-                    });
-
-                    k.onMouseDown((mouseBtn) => {
-                        if (mouseBtn !== "left" || player.isInDialogue) return;
-
-                        const worldMousePos = k.toWorld(k.mousePos());
-                        const diff = worldMousePos.sub(player.pos);
-
-
-                        if (Math.abs(diff.x) > Math.abs(diff.y)) {
-                            player.direction = diff.x > 0 ? "walk-right" : "walk-left";
-                        } else {
-                            player.direction = diff.y > 0 ? "walk-down" : "walk-up";
-                        }
-
-                        if (player.curAnim() !== player.direction) {
-                            player.play(player.direction);
-                        }
-                        player.moveTo(worldMousePos, player.speed);
-                    });
-
-                    k.onMouseRelease(() => {
-
-                        switch (player.direction) {
-                            case "walk-down":
-                                player.play("idle-down");
-                                break;
-                            case "walk-up":
-                                player.play("idle-up");
-                                break;
-                            case "walk-left":
-                                player.play("idle-left");
-                                break;
-                            case "walk-right":
-                                player.play("idle-right");
-                                break;
-                        }
-
-
-
-                    });
-
-                    const { text = '', photoUrl = '' }: TextWithPhoto = content.intro || {};
-
-                    displayDialogue(text, () => {
-                        player.isInDialogue = false;
-                    })
-
                 } catch (error) {
                     console.error("Failed to load map data:", error);
                 }
@@ -284,7 +254,7 @@ const KaboomComponent: React.FC = () => {
             <canvas ref={canvasRef} />
             {isVisible && (
                 <div className="absolute bottom-5 left-1/2 transform -translate-x-1/2 bg-white bg-opacity-80 text-black p-4 rounded-lg z-50 font-[monogram] text-2xl flex flex-col w-[80%] sm:w-[70%] md:w-[60%] lg:w-[50%]">
-                    {dialogueImg.length > 0 ? <img className="p-2.5" src={dialogueImg} /> : <></>}
+                    {dialogueImg.length > 0 ? <img className="p-2.5 max" src={dialogueImg} /> : <></>}
                     <div>
                         <span dangerouslySetInnerHTML={{ __html: dialogueText }} />
                     </div>
